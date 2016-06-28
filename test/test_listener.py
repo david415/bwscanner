1ff39883b733c3c6f7689e2e775300ef598fb1bc
David Stainton <dstainton415@gmail.com>
2016-06-28 02:16:43 +0000

    WIP

--- test/test_listener.py
+++ test/test_listener.py
@@ -1,3 +1,4 @@
+from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import defer, reactor
 from twisted.web.client import readBody
 from twisted.web.resource import Resource
@@ -5,8 +6,10 @@ from twisted.web.server import Site
 from twisted.trial.unittest import SkipTest
 from twisted.python import log
 from twisted.internet import task
+from twisted.internet.endpoints import clientFromString, connectProtocol, serverFromString
 from twisted.protocols.policies import WrappingFactory, ProtocolWrapper
 
+from twisted.trial import unittest
 from collections import deque
 from txtorcon.circuit import Circuit
 from txtorcon.util import available_tcp_port
@@ -24,6 +27,7 @@ class NotEnoughMeasurements(SkipTest):
 
 class DelayDeque(object):
     def __init__(self, maxlen, clock, handle_data):
+        print "delay deque init"
         self.maxlen = maxlen
         self.clock = clock
         self.handle_data = handle_data
@@ -35,21 +39,26 @@ class DelayDeque(object):
         self.lazy_tail = defer.succeed(None)
         self.clear_deque_timer = None
         self.clear_deque_duration = 10
+        self.fire_when_empty = defer.Deferred()
 
     def ready(self):
+        print "delay deque ready"
         self.is_ready = True
         self.stopped = False
         self.turn_deque()
 
     def pause(self):
+        print "delay deque pause"
         self.is_ready = False
         self.stopped = True
 
     def stop_all_timers(self):
+        print "delay deque stop all timers"
         if self.clear_deque_timer.active():
             self.clear_deque_timer.cancel()
 
     def append(self, data):
+        print "delay deque append"
         if self.clear_deque_timer is None:
             self.clear_deque_timer = self.clock.callLater(self.clear_deque_duration, self.deque.clear)
         else:
@@ -63,12 +72,14 @@ class DelayDeque(object):
             self.clock.callLater(0, self.turn_deque)
 
     def turn_deque(self):
+        print "delay deque turn deque"
         if self.stopped:
             return
         try:
             data = self.deque.pop()
         except IndexError:
             self.lazy_tail.addCallback(lambda ign: defer.succeed(None))
+            self.fire_when_empty.callback(None)
         else:
             self.lazy_tail.addCallback(lambda ign: self.handle_data(data))
             self.lazy_tail.addErrback(log.err)
@@ -80,29 +91,38 @@ class TrickleProtocol(ProtocolWrapper):
     """
 
     def write(self, data):
-        for b in data:
-            self.send_deque.append(b)
+        print "trickle protocol write"
+        ProtocolWrapper.dataReceived(self, data)
+        #for b in data:
+        #    self.send_deque.append(b)
 
     def writeSequence(self, seq):
+        print "trickle protocol write sequence"
         ProtocolWrapper.writeSequence(self, seq)
-        for data in seq:
-            self.write(data)
+        #for data in seq:
+        #    self.write(data)
 
     def dataReceived(self, data):
-        for b in data:
-            self.receive_deque.append(b)
+        print "trickle protocol data received"
+        ProtocolWrapper.dataReceived(self, data)
+        #for b in data:
+        #    self.receive_deque.append(b)
 
     def handleReceive(self, data):
+        print "trickel protocol handle received data"
         ProtocolWrapper.dataReceived(self, data)
 
     def handleSend(self, data):
+        print "tricket protocol handle send data"
         ProtocolWrapper.write(self, data)
 
     def registerProducer(self, producer, streaming):
+        print "trickle protocl register producer"
         self.producer = producer
         ProtocolWrapper.registerProducer(self, producer, streaming)
 
     def unregisterProducer(self):
+        print "trickle protocol unregister producer"
         del self.producer
         ProtocolWrapper.unregisterProducer(self)
 
@@ -114,18 +134,80 @@ class TrickleFactory(WrappingFactory):
     protocol = TrickleProtocol
 
     def __init__(self, wrappedFactory):
+        print "trickle factory init"
         WrappingFactory.__init__(self, wrappedFactory)
+        self.my_protocol = None
 
     def buildProtocol(self, addr):
+        print "trickle factory build protocol"
         self.my_protocol = WrappingFactory.buildProtocol(self, addr)
         # XXX
-        self.my_protocol.receive_deque = DelayDeque(100, reactor, self.my_protocol.handleReceive)
-        self.my_protocol.send_deque = DelayDeque(100, reactor, self.my_protocol.handleSend)
+        #self.my_protocol.receive_deque = DelayDeque(100, reactor, self.my_protocol.handleReceive)
+        #self.my_protocol.receive_deque.ready()
+        #self.my_protocol.send_deque = DelayDeque(100, reactor, self.my_protocol.handleSend)
+        #self.my_protocol.send_deque.ready()
         return self.my_protocol
 
     def unregisterProtocol(self, p):
+        print "trickle factory unregister protocol"
         WrappingFactory.unregisterProtocol(self, p)
 
+class DummyClientProtocol(Protocol):
+    def connectionMade(self):
+        self.transport.write("hello\r\n")
+    def dataReceived(self, data):
+        print "client received data: %s" % (data,)
+        #self.transport.loseConnection()
+
+class DummyClientFactory(Factory):
+    def buildProtocol(self, addr):
+        self.protocol = DummyClientProtocol()
+        return self.protocol
+
+class DummyServerFactory(Factory):
+    def buildProtocol(self, addr):
+        self.protocol = DummyServerProtocol()
+        return self.protocol
+
+class DummyServerProtocol(Protocol):
+    def connectionMade(self):
+        self.transport.write("hiya\r\n")
+    def dataReceived(self, data):
+        print "server received data: %s" % (data,)
+
+
+class TestTrickle(unittest.TestCase):
+    def test_blah(self):
+        def print_protocol(result):
+            print "print_protocol ---"
+            print result
+            return result
+        def print_failure(f):
+            print f
+            return f
+
+        #factory = TrickleFactory(server)
+        server_endpoint = serverFromString(reactor, "tcp:interface=127.0.0.1:8080")
+        server_factory = DummyServerFactory()
+        d = server_endpoint.listen(server_factory)
+        d.addCallback(print_protocol)
+        d.addErrback(print_failure)
+
+        client_endpoint = clientFromString(reactor, "tcp:127.0.0.1:8080")
+        client_factory = DummyClientFactory()
+        print "before connectProtocol"
+        d2 = client_endpoint.connect(client_factory)
+        d2.addCallback(print_protocol)
+        d2.addErrback(print_failure)
+
+        end_d = defer.DeferredList([d,d2])
+        def cleanup():
+            print "---- CLEANUP cleanup"
+            d.cancel()
+            client_factory.protocol.transport.loseConnection()
+            server_factory.protocol.transport.loseConnection()
+        end_d.addCallback(lambda ign: task.deferLater(reactor, 3, cleanup))
+        return end_d
 
 class FakeCircuit(Circuit):
     def __init__(self, id=None, state='BOGUS'):
@@ -181,7 +263,7 @@ class TestStreamBandwidthListener(TorTestCase):
         self.port = yield available_tcp_port(reactor)
         self.site = Site(DummyResource())
         self.factory = TrickleFactory(self.site)
-        self.test_service = yield reactor.listenTCP(self.port, self.factory)
+        self.test_service = reactor.listenTCP(self.port, self.factory)
 
         self.not_enough_measurements = NotEnoughMeasurements(
             "Not enough measurements to calculate STREAM_BW samples.")
