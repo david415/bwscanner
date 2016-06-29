533983a334461b7a91a9fdcf9803765c093e46e4
David Stainton <dstainton415@gmail.com>
2016-06-29 15:44:24 +0000

    fix trickle protocol wrapper tests and get rid of unused test code

--- test/test_listener.py
+++ test/test_listener.py
@@ -25,67 +25,6 @@ import time
 class NotEnoughMeasurements(SkipTest):
     pass
 
-class DelayDeque(object):
-    def __init__(self, maxlen, clock, handle_data):
-        print "delay deque init"
-        self.maxlen = maxlen
-        self.clock = clock
-        self.handle_data = handle_data
-
-        self.turn_delay = 0
-        self.deque = deque(maxlen=self.maxlen)
-        self.is_ready = False
-        self.stopped = False
-        self.lazy_tail = defer.succeed(None)
-        self.clear_deque_timer = None
-        self.clear_deque_duration = 10
-        self.fire_when_empty = defer.Deferred()
-
-    def ready(self):
-        print "delay deque ready"
-        self.is_ready = True
-        self.stopped = False
-        self.turn_deque()
-
-    def pause(self):
-        print "delay deque pause"
-        self.is_ready = False
-        self.stopped = True
-
-    def stop_all_timers(self):
-        print "delay deque stop all timers"
-        if self.clear_deque_timer.active():
-            self.clear_deque_timer.cancel()
-
-    def append(self, data):
-        print "delay deque append"
-        if self.clear_deque_timer is None:
-            self.clear_deque_timer = self.clock.callLater(self.clear_deque_duration, self.deque.clear)
-        else:
-            if self.clear_deque_timer.active():
-                self.clear_deque_timer.reset(self.clear_deque_duration)
-            else:
-                self.clear_deque_timer = self.clock.callLater(self.clear_deque_duration, self.deque.clear)
-
-        self.deque.append(data)
-        if self.is_ready:
-            self.clock.callLater(0, self.turn_deque)
-
-    def turn_deque(self):
-        print "delay deque turn deque"
-        if self.stopped:
-            return
-        try:
-            data = self.deque.pop()
-        except IndexError:
-            self.lazy_tail.addCallback(lambda ign: defer.succeed(None))
-            self.fire_when_empty.callback(None)
-        else:
-            self.lazy_tail.addCallback(lambda ign: self.handle_data(data))
-            self.lazy_tail.addErrback(log.err)
-            self.lazy_tail.addCallback(lambda ign: task.deferLater(self.clock, self.turn_delay, self.turn_deque))
-
-
 class DummyClientProtocol(Protocol):
     def connectionMade(self):
         self.transport.write("hello")
@@ -101,12 +40,24 @@ class DummyClientFactory(Factory):
         self.protocol.connection_lost_d = defer.Deferred()
         return self.protocol
 
-class TricklingProtocol(ProtocolWrapper):
+class TricklingBufferingProtocol(ProtocolWrapper):
 
-    def delayedWrite(self, data, offset=0, size=1, delay=.1):
+    def __init__(self, factory, wrappedProtocol):
+        ProtocolWrapper.__init__(self, factory, wrappedProtocol)
+        self.buffer = []
+        self.connection_lost_d = defer.Deferred()
+
+    def delayedWrite(self, data, offset=0, size=100, delay=.1):
         if offset >= len(data):
             return
-        b = data[offset:offset+size] # maybe replace with twisted.python.compat.lazyByteSlice if you seek py3 compatibility
+        if (offset + size) > len(data) -1:
+            b = data[offset:]
+            self.transport.write(b)
+            return
+        else:
+            # XXX
+            # maybe replace with twisted.python.compat.lazyByteSlice if you seek py3 compatibility
+            b = data[offset:offset+size]
         self.transport.write(b)
         reactor.callLater(delay, self.delayedWrite, data, offset+1, size, delay)
 
@@ -132,14 +83,23 @@ class TricklingProtocol(ProtocolWrapper):
         ProtocolWrapper.unregisterProducer(self)
 
 
-class DummyTricklingServerFactory(Factory):
-    def __init__(self):
+class TricklingWrappingFactory(Factory):
+
+    def __init__(self, wrapped_factory=None, wrapped_protocol=None):
         self.protocols = {}
+        self.wrapped_factory = wrapped_factory
+        self.wrapped_protocol = wrapped_protocol
+        self.my_protocol = None
 
     def buildProtocol(self, addr):
-        self.protocol = TricklingProtocol(self, DummyServerProtocol())
-        self.protocol.connection_lost_d = defer.Deferred()
-        return self.protocol
+        if self.wrapped_factory is not None and self.wrapped_protocol is None:
+            inner_protocol = self.wrapped_factory.buildProtocol(addr)
+            self.my_protocol = TricklingBufferingProtocol(self, inner_protocol)
+        else:
+            inner_protocol = self.wrapped_protocol
+            self.my_protocol = TricklingBufferingProtocol(self, inner_protocol)
+
+        return self.my_protocol
 
     def registerProtocol(self, p):
         """
@@ -163,7 +123,39 @@ class DummyServerProtocol(Protocol):
 class TestTrickle(unittest.TestCase):
     def test_blah(self):
         server_endpoint = serverFromString(reactor, "tcp:interface=127.0.0.1:8080")
-        server_factory = DummyTricklingServerFactory()
+        server_factory = TricklingWrappingFactory(wrapped_protocol=DummyServerProtocol())
+        d = server_endpoint.listen(server_factory)
+
+        client_endpoint = clientFromString(reactor, "tcp:127.0.0.1:8080")
+        client_factory = DummyClientFactory()
+        d2 = client_endpoint.connect(client_factory)
+
+        def fail_connect(f):
+            print "FAILURE %s" % (f,)
+            return f
+        d2.addErrback(fail_connect)
+
+        def stopListening(listeningPort):
+            return listeningPort.stopListening()
+        d.addCallback(lambda listeningPort: task.deferLater(reactor, 3, stopListening, listeningPort))
+
+        end_d = defer.DeferredList([d,d2])
+        def cleanup():
+            client_factory.protocol.transport.loseConnection()
+            lost_d = defer.DeferredList([client_factory.protocol.connection_lost_d, server_factory.my_protocol.connection_lost_d])
+            return lost_d
+        end_d.addCallback(lambda ign: task.deferLater(reactor, 3, cleanup))
+        return end_d
+
+    def test_http(self):
+        class DummyResource(Resource):
+            isLeaf = True
+            def render_GET(self, request):
+                return 'a' * fetch_size
+
+        server_endpoint = serverFromString(reactor, "tcp:interface=127.0.0.1:8080")
+        site_factory = Site(DummyResource())
+        server_factory = TricklingWrappingFactory(wrapped_factory=site_factory)
         d = server_endpoint.listen(server_factory)
 
         client_endpoint = clientFromString(reactor, "tcp:127.0.0.1:8080")
@@ -171,14 +163,14 @@ class TestTrickle(unittest.TestCase):
         d2 = client_endpoint.connect(client_factory)
 
         def stopListening(listeningPort):
-            return listeningPort.stopListening()    
+            return listeningPort.stopListening()
         d.addCallback(lambda listeningPort: task.deferLater(reactor, 3, stopListening, listeningPort))
 
         end_d = defer.DeferredList([d,d2])
         def cleanup():
             client_factory.protocol.transport.loseConnection()
-            lost_d = defer.DeferredList([client_factory.protocol.connection_lost_d, server_factory.protocol.connection_lost_d])
-            return lost_d
+            #lost_d = defer.DeferredList([client_factory.protocol.connection_lost_d, server_factory.protocol.connection_lost_d])
+            #return lost_d
         end_d.addCallback(lambda ign: task.deferLater(reactor, 3, cleanup))
         return end_d
 
@@ -195,6 +187,11 @@ class TestCircuitEventListener(TorTestCase):
     @defer.inlineCallbacks
     def setUp(self):
         yield super(TestCircuitEventListener, self).setUp()
+
+        # XXX - this looks sloppy that we pass self.tor to the
+        # CircuitEventListener as well as pass that reference
+        # to a method call on self.tor; maybe like we could
+        # do with one less reference to self.tor
         self.circuit_event_listener = CircuitEventListener(self.tor)
         self.tor.add_circuit_listener(self.circuit_event_listener)
 
@@ -223,7 +220,7 @@ class TestStreamBandwidthListener(TorTestCase):
     def setUp(self):
         yield super(TestStreamBandwidthListener, self).setUp()
         #self.fetch_size = 8*2**20  # 8MB
-        self.fetch_size = 100  # 8MB
+        self.fetch_size = 100
         fetch_size = self.fetch_size
         self.stream_bandwidth_listener = yield StreamBandwidthListener(self.tor)
 
@@ -235,7 +232,7 @@ class TestStreamBandwidthListener(TorTestCase):
 
         self.port = yield available_tcp_port(reactor)
         self.site = Site(DummyResource())
-        self.factory = TrickleFactory(self.site)
+        self.factory = TricklingWrappingFactory(self.site)
         self.test_service = reactor.listenTCP(self.port, self.factory)
 
         self.not_enough_measurements = NotEnoughMeasurements(
@@ -247,7 +244,7 @@ class TestStreamBandwidthListener(TorTestCase):
         r = yield self.do_fetch()
         print "before bw_events"
         bw_events = self.stream_bandwidth_listener.circ_bw_events.get(r['circ'])
-      
+
         #def yo(result):
         #    print "YOIYOIYOYOYOYOOY"
         #self.factory.my_protocol.receive_d.addCallback(yo)
